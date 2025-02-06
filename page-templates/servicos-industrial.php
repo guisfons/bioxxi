fcfef4c1c01aa30f7e17d35a2a426f2dfbfaed1f
guisfons <guilhermesfonsecaa@gmail.com>
2025-02-06 10:37:31 -0300

    Section Métodos de Esterilização

--- page-templates/servicos-industrial.php
+++ page-templates/servicos-industrial.php
@@ -40,14 +40,27 @@
     </section>
 <?php endif; ?>
 
-<section class="metodo_esterilizacao">
-    <figure><img src="<?php echo esc_url(get_home_url(get_template_directory_uri())); ?>/assets/img/icons/double-arrow.svg" alt="Ornamento"></figure>
-    <article></article>
-    <figure><img src="<?php echo esc_url(get_home_url(get_template_directory_uri())); ?>/assets/img/icons/double-arrow.svg" alt="Ornamento"></figure>
+<section class="metodo__esterilizacao">
+    <div class="wrapper">
+        <figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/icons/double-symbol.svg" alt="Ornamento"></figure>
+        <div class="metodo__esterilizacao-container">
+            <h2><?= get_field('titulo_reprocessamento'); ?></h2>
+            <?php if (have_rows('metodos_de_esterilizacao')): ?>
+                <?php while (have_rows('metodos_de_esterilizacao')): the_row(); ?>
+                    <article>
+                        <figure><?php echo wp_get_attachment_image(get_sub_field('icone'), 'full'); ?></figure>
+                        <span><?php echo get_sub_field('titulo'); ?></span>
+                    </article>
+                <?php endwhile; ?>
+            <?php endif; ?>
+
+            <article><?php echo get_field('saiba_mais'); ?></article>
+        </div>
+        <figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/icons/double-symbol.svg" alt="Ornamento"></figure>
+    </div>
 </section>
 
 <?php get_template_part('template-parts/reprocessamento/cards'); ?>
-
 <?php get_template_part('template-parts/form'); ?>
 <?php
     get_footer();
