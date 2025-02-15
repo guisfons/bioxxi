19a1c8e877c4427091af7855823215433f05203e
guisfons <guilhermesfonsecaa@gmail.com>
2025-02-14 21:04:59 -0300

    header menu and container width fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+$( document ).ready(function() {
+    $('.header__trigger').on('click', function() {
+        $(this).toggleClass('header__trigger--active');
+        $('.menu-menu-container').toggleClass('menu-menu-container--active');
+    });
+})
+
 $('img').on('dragstart', function(event) { event.preventDefault(); });
